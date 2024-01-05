b70142cf908769f885e11bb49be21bad6159c3c5
Continentalbinly <113883031+Continentalbinly@users.noreply.github.com>
2024-01-06 00:54:15 +0700

    ReComponent Add asset

--- app/src/main/java/com/example/acledaexam/LoginActivity.java
+++ app/src/main/java/com/example/acledaexam/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.acledaexam;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,20 +10,21 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-    private static final String MOCK_USERNAME = "Test";
-    private static final String MOCK_PASSWORD = "Test";
+    private static final String MOCK_USERNAME = "test";
+    private static final String MOCK_PASSWORD = "test";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button loginButton = findViewById(R.id.loginButton);
+        Button loginButton = findViewById(R.id.btnSignIn);
+        loginButton.setBackgroundColor(Color.parseColor("#EDBE3C"));
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Mocking user input
-                EditText usernameEditText = findViewById(R.id.usernameEditText);
-                EditText passwordEditText = findViewById(R.id.passwordEditText);
+                EditText usernameEditText = findViewById(R.id.edtUser);
+                EditText passwordEditText = findViewById(R.id.edtPass);
 
                 String enteredUsername = usernameEditText.getText().toString().trim();
                 String enteredPassword = passwordEditText.getText().toString().trim();
